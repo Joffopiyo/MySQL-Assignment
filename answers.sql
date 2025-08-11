de01d5183bbe077f0191530aec66794d75978cb6
Joffopiyo <joffopiyo@gmail.com>
2025-08-11 11:06:53 +0300

    Added answers.sql for final submission

---
+++ answers.sql
@@ -0,0 +1,25 @@
+-- Query 1
+SELECT checkNumber, paymentDate, amount
+FROM payments;
+
+-- Query 2
+SELECT orderDate, requiredDate, status
+FROM orders
+WHERE status = 'In Process'
+ORDER BY orderDate DESC;
+
+-- Query 3
+SELECT firstName, lastName, email
+FROM employees
+WHERE jobTitle = 'Sales Rep'
+ORDER BY employeeNumber DESC;
+
+-- Query 4
+SELECT *
+FROM offices;
+
+-- Query 5
+SELECT productName, quantityInStock
+FROM products
+ORDER BY buyPrice ASC
+LIMIT 5;
